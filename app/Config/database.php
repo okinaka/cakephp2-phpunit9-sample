df70d74413cff4f77980cd85a6b3e657b12479a3
Kenshin Okinaka <okinaka.kenshin@binc.jp>
2022-11-11 12:08:46 +0900

    update database.php

--- app/Config/database.php
+++ app/Config/database.php
@@ -67,24 +67,18 @@
 class DATABASE_CONFIG {
 
 	public $default = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'database_name',
+		'datasource' => 'Database/Sqlite',
+		'persistent' => true,
+		'database' => ':memory:',
 		'prefix' => '',
-		//'encoding' => 'utf8',
+		'encoding' => 'utf8',
 	);
 
 	public $test = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'test_database_name',
+		'datasource' => 'Database/Sqlite',
+		'persistent' => true,
+		'database' => ':memory:',
 		'prefix' => '',
-		//'encoding' => 'utf8',
+		'encoding' => 'utf8',
 	);
 }
